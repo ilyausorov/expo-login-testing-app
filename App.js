d7f2af3cd89538cb6fad311552bd4af19bf18f6b
ilya usorov <ilya.usorov@gmail.com>
2020-04-13 20:57:30 -0400

    initial commit

---
+++ App.js
@@ -0,0 +1,56 @@
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+
+import * as Facebook from 'expo-facebook';
+
+const loginWithFacebook = async () => {
+  try {
+    const response = await Facebook.logInWithReadPermissionsAsync({
+      permissions: ['public_profile'],
+    });
+
+    const {
+      type,
+      token,
+      expires,
+      permissions,
+      declinedPermissions,
+    } = response;
+
+    if (type === 'success') {
+      // Get the user's name using Facebook's Graph API
+      const user = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
+      Alert.alert('Logged in!', `Hi ${(await user.json()).name}!`);
+    } else {
+      Alert.alert('Login failed!');
+    }
+  } catch ({ message }) {
+    alert(`Facebook Login Error: ${message}`);
+  }
+}
+
+export default function App() {
+
+  useEffect(() => {
+    const initFacebook = async () => {
+      await Facebook.initializeAsync("263384934828090", "Expo-Login-Testing-App")
+    }
+    initFacebook()
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text>Expo Login Testing App: Facebook</Text>
+      <Button onPress={loginWithFacebook} title="Press Me" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
